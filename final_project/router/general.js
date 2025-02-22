552993f86fbd19b755d66f15bfdd37766033f782
Maris Feagin <mfeagin64@gmail.com>
2025-02-22 00:26:09 -0500

    task 5 completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,7 +17,7 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn = req.params.key;
+  const isbn = req.params.id;
   res.send(books[isbn]);
  });
   
@@ -59,8 +59,14 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.id;
+  const book = books[isbn]; // Assuming 'books' is your data object
+
+  if (book && book.reviews) {
+    res.json(book.reviews);
+  } else {
+    res.status(404).send('Reviews not found for this ISBN');
+  }
 });
 
 module.exports.general = public_users;
